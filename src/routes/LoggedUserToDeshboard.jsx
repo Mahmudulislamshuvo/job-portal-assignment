10656597c33c772a9afc395feee0d593b55d2f36
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-01-11 20:24:01 +0600

    feat: Refactor LoggedUserToDeshboard to use useEffect for navigation based on user role

--- src/routes/LoggedUserToDeshboard.jsx
+++ src/routes/LoggedUserToDeshboard.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const LoggedUserToDeshboard = () => {
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
-  if (user?.role === "USER") {
-    navigate("/user-dashboard");
-    return;
-  } else if (user?.role === "COMPANY") {
-    navigate("/company-dashboard");
-    return;
-  }
+  useEffect(() => {
+    if (user?.role === "USER") {
+      navigate("/user-dashboard");
+    } else if (user?.role === "COMPANY") {
+      navigate("/company-dashboard");
+    }
+  }, [user, navigate]);
 
   return (
     <>
