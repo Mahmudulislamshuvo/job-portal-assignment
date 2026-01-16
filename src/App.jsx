102d6088bb665752967b99f7bc4b742751fbc2fc
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-01-17 03:34:17 +0600

    feat: Move user profile route to allow access for both users and companies

--- src/App.jsx
+++ src/App.jsx
@@ -39,12 +39,12 @@ const App = () => {
           <Route element={<UserPrivateLayout />}>
             {/* Only for users */}
             <Route path="/user-dashboard" element={<UserDashboard />} />
-            <Route path="/user-profile" element={<UserProfile />} />
             <Route path="/edit-user-profile" element={<EditUserProfile />} />
             <Route path="/applied-jobs" element={<AppliedJobs />} />
           </Route>
           {/* Company and user both can access start */}
           <Route path="/company/profile/:slug" element={<CompanyProfle />} />
+          <Route path="/user-profile" element={<UserProfile />} />
           {/* Company and user both can access End */}
           <Route element={<CompanyPrivateLayout />}>
             {/* only for company */}
